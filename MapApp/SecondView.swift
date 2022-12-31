d78378255ed8cad5126059c188baf3cb27b8fed1
Uzu Lee <chris0803lee@gmail.com>
2022-12-30 22:54:30 -0800

    Added second view

---
+++ MapApp/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  MapApp
+//
+//  Created by Mac-aroni on 12/30/22.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("Hello, World! ")
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
